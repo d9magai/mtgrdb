feba20a8f434dd1da00dd3abfe9286b32c0c39de
d9magai <kumagai@sencorp.co.jp>
2017-07-22 01:16:32 +0900

    add update sql files

---
+++ update8eb.sql
@@ -0,0 +1,8 @@
+UPDATE cards SET mcinumber='1', setcode='8EB' WHERE id='0c354bb03991d149b9fa24c2941ed047e5eaea65';
+UPDATE cards SET mcinumber='6', setcode='8EB' WHERE id='aaf4c8d27a30aaba3029e3bb98783e2eac115f8d';
+UPDATE cards SET mcinumber='3', setcode='8EB' WHERE id='a32cc84604cd92fd8a8ceb52cfdd511b1933de58';
+UPDATE cards SET mcinumber='4', setcode='8EB' WHERE id='ea6bba794e7c477fbcd33b5ea21cb2c03b915bf6';
+UPDATE cards SET mcinumber='7', setcode='8EB' WHERE id='d6f98c6075c8c5696ac214b595efecd178eb3a47';
+UPDATE cards SET mcinumber='2', setcode='8EB' WHERE id='7aac4a9a3da01359a90fe06a679b1d05f7445930';
+UPDATE cards SET mcinumber='5', setcode='8EB' WHERE id='bb810207b0021a4efc71f7e1e9b134353c270ade';
+
