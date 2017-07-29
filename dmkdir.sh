6224b986a48a67215e801ffa0e36d38d898027d5
d9magai <kumagai@sencorp.co.jp>
2017-07-29 09:33:44 +0900

    add files

---
+++ dmkdir.sh
@@ -0,0 +1,142 @@
+rm -rf  LEA
+rm -rf  LEB
+rm -rf  CED
+rm -rf  2ED
+rm -rf  CEI
+rm -rf  ARN
+rm -rf  pDRC
+rm -rf  ATQ
+rm -rf  3ED
+rm -rf  LEG
+rm -rf  DRK
+rm -rf  FEM
+rm -rf  pLGM
+rm -rf  pMEI
+rm -rf  4ED
+rm -rf  ICE
+rm -rf  CHR
+rm -rf  HML
+rm -rf  ALL
+rm -rf  RQS
+rm -rf  pARL
+rm -rf  pCEL
+rm -rf  MIR
+rm -rf  MGB
+rm -rf  ITP
+rm -rf  VIS
+rm -rf  5ED
+rm -rf  VAN
+rm -rf  pPOD
+rm -rf  POR
+rm -rf  WTH
+rm -rf  pPRE
+rm -rf  TMP
+rm -rf  STH
+rm -rf  pJGP
+rm -rf  EXO
+rm -rf  PO2
+rm -rf  UGL
+rm -rf  pALP
+rm -rf  USG
+rm -rf  ATH
+rm -rf  ULG
+rm -rf  6ED
+rm -rf  PTK
+rm -rf  UDS
+rm -rf  pGRU
+rm -rf  S99
+rm -rf  pWOR
+rm -rf  pWOS
+rm -rf  MMQ
+rm -rf  BRB
+rm -rf  pSUS
+rm -rf  pFNM
+rm -rf  pELP
+rm -rf  NMS
+rm -rf  S00
+rm -rf  PCY
+rm -rf  BTD
+rm -rf  INV
+rm -rf  PLS
+rm -rf  7ED
+rm -rf  pMPR
+rm -rf  APC
+rm -rf  ODY
+rm -rf  DKM
+rm -rf  TOR
+rm -rf  JUD
+rm -rf  ONS
+rm -rf  LGN
+rm -rf  SCG
+
+mkdir LEA
+mkdir LEB
+mkdir CED
+mkdir 2ED
+mkdir CEI
+mkdir ARN
+mkdir pDRC
+mkdir ATQ
+mkdir 3ED
+mkdir LEG
+mkdir DRK
+mkdir FEM
+mkdir pLGM
+mkdir pMEI
+mkdir 4ED
+mkdir ICE
+mkdir CHR
+mkdir HML
+mkdir ALL
+mkdir RQS
+mkdir pARL
+mkdir pCEL
+mkdir MIR
+mkdir MGB
+mkdir ITP
+mkdir VIS
+mkdir 5ED
+mkdir VAN
+mkdir pPOD
+mkdir POR
+mkdir WTH
+mkdir pPRE
+mkdir TMP
+mkdir STH
+mkdir pJGP
+mkdir EXO
+mkdir PO2
+mkdir UGL
+mkdir pALP
+mkdir USG
+mkdir ATH
+mkdir ULG
+mkdir 6ED
+mkdir PTK
+mkdir UDS
+mkdir pGRU
+mkdir S99
+mkdir pWOR
+mkdir pWOS
+mkdir MMQ
+mkdir BRB
+mkdir pSUS
+mkdir pFNM
+mkdir pELP
+mkdir NMS
+mkdir S00
+mkdir PCY
+mkdir BTD
+mkdir INV
+mkdir PLS
+mkdir 7ED
+mkdir pMPR
+mkdir APC
+mkdir ODY
+mkdir DKM
+mkdir TOR
+mkdir JUD
+mkdir ONS
+mkdir LGN
+mkdir SCG
+
