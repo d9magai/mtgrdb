b65a1ba21320713894495e3d86cb082e912cd246
d9magai <kumagai@sencorp.co.jp>
2018-02-26 22:35:06 +0900

    implements DownloadJob

---
+++ downloader.py
@@ -0,0 +1,44 @@
+from string import punctuation
+
+from constant import EXTRAS_CARDTYPE
+from constant import EXTRAS_PATH
+from constant import GATHERER_WIZARDS_ENDPOINT
+from constant import MAGICCARDSINFO_TABLE
+from constant import MAGICCARDS_ENDPOINT
+
+
+class DownloadJob(object):
+
+    def __init__(self, dic):
+        self.__url = self.__get_url(dic)
+
+    def url(self):
+        return self.__url
+
+    def __get_extras_name_format(self, name):
+        t = str.maketrans('', '', punctuation)
+        return name.replace("-", " ").translate(t).lower().replace(" ", "-")
+
+    def __get_url(self, dic):
+        one_type = dic['types'][0] if isinstance(dic['types'], list) else None
+
+        if one_type in EXTRAS_CARDTYPE:
+            url = '{}/extras/{}/{}.jpg'.format(
+                MAGICCARDS_ENDPOINT,
+                EXTRAS_PATH[one_type + dic['setcode']],
+                self.__get_extras_name_format(dic['name']),
+            )
+            return url
+
+        if one_type == 'Vanguard' or not dic['mcinumber']:
+            url = '{}?multiverseid={}&type=card'.format(
+                GATHERER_WIZARDS_ENDPOINT,
+                dic['multiverseid'],
+            )
+            return url
+
+        return '{}/scans/en/{}/{}.jpg'.format(
+            MAGICCARDS_ENDPOINT,
+            MAGICCARDSINFO_TABLE.get(dic['setcode'], dic['setcode'].lower()),
+            dic['mcinumber'],
+        )
